173fd1de690291e7f312c67ad6dd14826813f19e
Rainer Poisel <rainer.poisel@gmail.com>
2015-10-11 23:34:27 +0200

    confirmation dialog added

---
+++ static/app/js/directives.js
@@ -0,0 +1,15 @@
+/* see http://stackoverflow.com/questions/18313576/confirmation-dialog-on-ng-click-angularjs */
+angular.module('demoapp').directive('ngConfirmClick', [
+    function(){
+        return {
+            link: function (scope, element, attr) {
+                var msg = attr.ngConfirmClick || "Are you sure?";
+                var clickAction = attr.confirmedClick;
+                element.bind('click',function (event) {
+                    if ( window.confirm(msg) ) {
+                        scope.$eval(clickAction)
+                    }
+                });
+            }
+        };
+    }]);
