b55f9f545227e2c7dbd43bd617aacefd20832325
Rainer Poisel <rainer.poisel@gmail.com>
2015-10-27 23:08:50 +0000

    control flow with promises for e2e tests

--- static/tests/veloprovements-spec.js
+++ static/tests/veloprovements-spec.js
@@ -34,38 +34,51 @@ function getRandomArbitrary(min, max) {
         return Math.random() * (max - min) + min;
 }
 
-var loginAsTestUser = function(userAction, login_username, login_password, login_submit) {
-    var userAction = element(by.id('userAction'));
-    var login_username = element(by.id('login_username'));
-    var login_password = element(by.id('login_password'));
-    var login_submit = element(by.id('login_submit'));
+describe('veloprovements', function() {
+    var loginAsTestUser = function(userAction, login_username, login_password, login_submit) {
+        var defer = protractor.promise.defer();
 
-    return userAction.click().then(function() {
+        var userAction = element(by.id('userAction'));
+        var login_username = element(by.id('login_username'));
+        var login_password = element(by.id('login_password'));
+        var login_submit = element(by.id('login_submit'));
+
+        userAction.click().then(function() {
             login_username.sendKeys('test');
             login_password.sendKeys('test');
-            login_submit.click()
-    });
-};
+            login_submit.click();
+            waitForPanel();
+            defer.fulfill();
+        });
 
-var drawPoint = function(map, x, y, name, description) {
-    var drawMarker = element(by.css('.leaflet-draw-draw-marker'));
-    var elemName = element(by.id('createVeloprovementName'));
-    var elemDescription = element(by.id('createVeloprovementDescription'));
-    var elemSubmit = element(by.id('createVeloprovementSubmit'));
+        return defer.promise;
+    };
 
-    drawMarker.click();
-    browser.actions().mouseMove(map, {x: x, y: y}) .click().perform();
-    waitForPanel();
-    elemName.sendKeys(name);
-    elemDescription.sendKeys(description);
-    return elemSubmit.click();
-};
+    var drawPoint = function(map, x, y, name, description) {
+        var defer = protractor.promise.defer();
 
-var waitForPanel = function() {
-    browser.driver.sleep(500);
-};
+        var drawMarker = element(by.css('.leaflet-draw-draw-marker'));
+        var elemName = element(by.id('createVeloprovementName'));
+        var elemDescription = element(by.id('createVeloprovementDescription'));
+        var elemSubmit = element(by.id('createVeloprovementSubmit'));
 
-describe('veloprovements', function() {
+        drawMarker.click().then(function() {
+            browser.actions().mouseMove(map, {x: x, y: y}) .click().perform();
+            waitForPanel();
+            elemName.sendKeys(name);
+            elemDescription.sendKeys(description);
+            elemSubmit.click();
+            waitForPanel();
+            defer.fulfill();
+        });
+
+        return defer.promise;
+    };
+
+    var waitForPanel = function() {
+        browser.driver.sleep(500);
+    };
+
     it('should have the correct title', function() {
         browser.get(baseUrl);
 
@@ -74,25 +87,38 @@ describe('veloprovements', function() {
 
     it('create veloprovement as test user', function(done) {
 
+        var flow = protractor.promise.controlFlow();
+        var numPoints = 10;
         var map = element(by.id('veloprovementsmap'));
         var lenBefore = 0, lenAfter = 0;
         var geoQueryUrl = baseUrl + '/dynamic/veloprovements?southWestLat=48.19348500446728&southWestLng=15.616292953491211&northEastLat=48.20651434072251&northEastLng=15.643694400787352';
 
-        httpsGet(geoQueryUrl).then(function(result) {
-            lenBefore = JSON.parse(result.bodyString).features.length;
-            loginAsTestUser().then(function() {
-                    waitForPanel();
-                    map.getSize().then(function(boundingBox) {
-                    drawPoint(map, getRandomArbitrary(50, boundingBox.width), getRandomArbitrary(50, boundingBox.height), 'Protractor', 'was here').then(function() {
-                            httpsGet(geoQueryUrl).then(function(result) {
-                                lenAfter = JSON.parse(result.bodyString).features.length;
-                                expect(lenAfter).toEqual(lenBefore + 1);
-                                done();
-                            });
-                        });
-                    });
+        var drawRandomPoint = function() {
+            map.getSize().then(function(boundingBox) {
+                drawPoint(map, getRandomArbitrary(50, boundingBox.width), getRandomArbitrary(50, boundingBox.height), 'Protractor', 'was here');
             });
-        });
+        };
+
+        var determineLenBefore = function() {
+            httpsGet(geoQueryUrl).then(function(result) {
+                lenBefore = JSON.parse(result.bodyString).features.length;
+            });
+        };
+
+        var determineLenAfter = function() {
+            httpsGet(geoQueryUrl).then(function(result) {
+                lenAfter = JSON.parse(result.bodyString).features.length;
+                expect(lenAfter).toEqual(lenBefore + numPoints);
+                done();
+            });
+        };
+
+        flow.execute(determineLenBefore);
+        flow.execute(loginAsTestUser);
+        for (var i = 0; i < numPoints; i++) {
+            flow.execute(drawRandomPoint);
+        }
+        flow.execute(determineLenAfter);
     });
 });
 
